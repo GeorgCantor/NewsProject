16895e9c2928194ba726f314987c4c3afbac1bc2
GeorgCantor <cupsman1986@gmail.com>
2019-12-15 13:56:12 +0300

    added menu for open drawer

--- app/src/main/java/com/georgcantor/newsproject/view/MainActivity.kt
+++ app/src/main/java/com/georgcantor/newsproject/view/MainActivity.kt
@@ -2,8 +2,10 @@ package com.georgcantor.newsproject.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation
 import com.georgcantor.newsproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Navigation.findNavController(this, R.id.navHostFragment).navigate(R.id.tabsFragment)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 }
--- app/src/main/java/com/georgcantor/newsproject/view/fragment/TabsFragment.kt
+++ app/src/main/java/com/georgcantor/newsproject/view/fragment/TabsFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.georgcantor.newsproject.R
 import com.georgcantor.newsproject.view.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_tabs.*
 
 class TabsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +26,12 @@ class TabsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        toolbar.title = getString(R.string.app_name)
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        toolbar.setNavigationOnClickListener {
+            requireActivity().drawerLayout.openDrawer(GravityCompat.START)
+        }
+        toolbar.inflateMenu(R.menu.menu_main)
         tabLayout.setupWithViewPager(viewPager)
 
         val adapter = ViewPagerAdapter(childFragmentManager)
